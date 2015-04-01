05500c384721b0da27962c68e7f65059486917f0
fuqi <fuqi7758521@qq.com>
2015-04-01 18:06:19 +0800

    weisha

--- src/com/hofuqi/Person.java
+++ src/com/hofuqi/Person.java
@@ -3,27 +3,34 @@ package com.hofuqi;
 public class Person {
 	private String name;
 	private int age;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public Person(String name, int age){
+
+	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
-	public Person(){}
-	
-	public void show(){
-		System.out.println("name: " + this.name);
-		System.out.println("age: " + this.age);
+
+	public Person() {
 	}
-	
+
+	public void show() {
+		System.out.println("name: " + this.name + " age:" + this.age);
+		// System.out.println("age: " + this.age);
+	}
+
 }
